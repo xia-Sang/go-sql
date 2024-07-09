1b0bae5fb3844806d5c3b9a17900803198101267
xia-sang <3188674636@qq.com>
2024-07-10 00:08:43 +0800

    ch1

--- repl/parse.go
+++ repl/parse.go
@@ -2,16 +2,26 @@ package repl
 
 import (
 	"bufio"
+	"bytes"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 )
+var (
+	EXIT                  = errors.New("Exit")
+	ERROR_COMMAND         = errors.New("error command")
+	ERROR_BUFFER_OVERFLOW = errors.New("input exceeds buffer size")
+)
 
-const MAX_BUFFER_SIZE = 512
+const (
+	MAX_BUFFER_SIZE = 512
+)
 
 type Scanner struct {
 	buffer []byte
 	index  int
+	length int
 	line   int
 }
 
@@ -19,66 +29,121 @@ func newScanner() *Scanner {
 	return &Scanner{
 		buffer: make([]byte, MAX_BUFFER_SIZE),
 		index:  0,
-		line:   0,
+		line:   1,
 	}
 }
 
-var (
-	EXIT = errors.New("Exit")
-)
-
-func (s *Scanner) readInput() error {
+func showDb(){
+	fd,err:=os.Open("./repl/show.txt")
+	if err!=nil{
+		panic(err)
+	}
+	defer fd.Close()
+	data,err:=io.ReadAll(fd)
+	if err!=nil{
+		panic(err)
+	}
+	fmt.Printf("%s\n",data)
+}
+// 读取新的一行
+func (s *Scanner) readNewLine() error {
 	reader := bufio.NewReader(os.Stdin)
-	reader.ReadByte('.')
-	line, err := reader.ReadBytes(';')
+	line, _, err := reader.ReadLine()
 	if err != nil {
 		os.Exit(1)
 	}
-	if len(line) > MAX_BUFFER_SIZE {
-		return errors.New("")
+	length := len(line)
+	if length > MAX_BUFFER_SIZE {
+		return ERROR_BUFFER_OVERFLOW
 	}
 	s.buffer = line
+	s.length = length
 	s.line += 1
 	s.index = 0
+
 	return nil
 }
-func (s *Scanner) dealCommand() error {
-	if s.index == 0 {
-		if s.buffer[s.index] == '.' {
-			if s.buffer[s.index+1] == 'e' && s.buffer[s.index+2] == 'x' && s.buffer[s.index+3] == 'i' && s.buffer[s.index+4] == 't' {
-				return EXIT
-			}
-			return nil
-		}
-		return nil
+
+func (s *Scanner) dotCommand() error {
+	if bytes.Equal(s.buffer[s.index:s.index+5], []byte(".exit")) {
+		return EXIT
 	}
-	fmt.Printf("Unrecognized command '%s'.\n", s.buffer)
-	return nil
+	return ERROR_COMMAND
 }
-func printPrompt() {
-	fmt.Print("db> ")
-}
-func dealCommand() {
+// 多行命令 获取next
+func (s *Scanner) next() (data []byte) {
+    prev := s.index
 
+	for prev < s.length && s.buffer[prev] == ' ' {
+        prev++
+    }
+
+    s.index = prev
+    for prev < s.length && s.buffer[prev] != ' ' {
+        prev++
+    }
+    data = (s.buffer[s.index:prev])
+
+    for prev < s.length && s.buffer[prev] == ' ' {
+        prev++
+    }
+    s.index = prev
+    return data
 }
+
+func (s *Scanner) normalCommand() error {
+	line := ""
+	index:=1
+	flag:=false
+	for s.index < s.length {
+		currCommand:=string(s.next())
+		if index==1{
+			if currCommand=="select" || currCommand=="insert"{
+				flag=true
+			}else{
+				//todo
+			}
+		}
+		data := currCommand+" "
+		line += data
+	}
+	// fmt.Println(line)
+	if flag{
+		fmt.Println("Executed.")
+		return nil
+	}else{
+		fmt.Printf("Unrecognized command '%s'.\n", line)
+		return ERROR_COMMAND
+	}
+}
+
+func (s *Scanner) dealCommand() error {
+	if s.index == 0 && s.buffer[s.index] == '.' {
+		return s.dotCommand()
+	} else {
+		return s.normalCommand()
+	}
+}
+
+func (s *Scanner) printPrompt() {
+	fmt.Printf("db:%d> ", s.line)
+}
+
 func ParseCommand() {
-	// args := os.Args
-
-	// if len(args) < 2 {
-	// 	return
-	// }
-
-	// for i, arg := range args[1:] {
-	// 	fmt.Printf("Argument %d: %s\n", i+1, arg)
-	// }
+	showDb()
 	scan := newScanner()
 	for {
-		printPrompt()
-		scan.readInput()
+		scan.printPrompt()
+		err := scan.readNewLine()
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+			continue
+		}
 		if err := scan.dealCommand(); err != nil {
 			if err == EXIT {
 				break
 			}
+			fmt.Println("Error:", err)
 		}
 	}
 }
